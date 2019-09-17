4e5de761ada87bac4631da08a9a51f2179dfb20d
dejan366 <35435456+dejan366@users.noreply.github.com>
2019-09-17 13:00:03 +0200

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,5 +1,9 @@
+function refresh(){
+	setTimeout(function(){ location.reload(true); }, 300);
+}
 	function klik(a,b,c){
 	xmlhttp = new XMLHttpRequest();
         xmlhttp.open("GET","index.php?number1="+a+"&number2="+c+"&operation="+b,true);
         xmlhttp.send(null);
+		refresh();
 }
